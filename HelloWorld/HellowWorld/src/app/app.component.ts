48353602589d3d50bc6fd9704f30b540c6c04cd0
Joseph Escue <joseph.escue@comcast.net>
2019-05-21 21:30:33 -0500

    Edited title

--- HelloWorld/HellowWorld/src/app/app.component.ts
+++ HelloWorld/HellowWorld/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular Tutorial 0001 - Hello World';
+  title = 'Hello World';
 }
