da844828b0ae443b29c0e1e3cdb12dcf40c3087b
Joseph Escue <joseph.escue@comcast.net>
2019-06-07 18:04:18 -0500

    Lesson 15 - Component Interaction

---
+++ ComInteraction/ComInteraction/src/app/test/test.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+    <h2>{{"Hello " + name}}</h2>
+    <button (click)="fireEvent()">Send Event</button>
+  `,
+  styles: []
+})
+export class TestComponent implements OnInit {
+
+// tslint:disable-next-line: no-input-rename
+  @Input('parentData') public name;
+  @Output() public childEvent = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  fireEvent(){
+    this.childEvent.emit('Hey Codevolution');
+  }
+
+}
