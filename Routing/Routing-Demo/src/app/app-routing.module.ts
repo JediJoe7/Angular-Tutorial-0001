b036dc25aa819c1cb5cd32d840c1ca371297b3c1
Joseph Escue <joseph.escue@comcast.net>
2019-06-10 17:08:52 -0500

    Lesson 24 - Wildcard Routes

--- Routing/Routing-Demo/src/app/app-routing.module.ts
+++ Routing/Routing-Demo/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DepartmentListComponent } from './department-list/department-list.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+// NOTE:  the ordering of the wildcard route doesn't seem to work unless it's the second element */
 const routes: Routes = [
   { path: 'departments', component: DepartmentListComponent},
-  { path: 'employees', component: EmployeeListComponent}
+  { path: '', redirectTo: 'departments', pathMatch: 'full'},
+  { path: 'employees', component: EmployeeListComponent},
+  { path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
@@ -13,4 +17,6 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [DepartmentListComponent, EmployeeListComponent];
+export const routingComponents = [DepartmentListComponent,
+                                 EmployeeListComponent,
+                                 PageNotFoundComponent];
