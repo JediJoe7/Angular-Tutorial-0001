334c5c62c2a6fbbde660197c2e9fe24cbcf28912
Joseph Escue <joseph.escue@comcast.net>
2019-06-11 09:52:01 -0500

    Lesson 26 - Parameter Mapping

--- RoutingParameters/RouteParms/src/app/department-detail/department-detail.component.ts
+++ RoutingParameters/RouteParms/src/app/department-detail/department-detail.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-department-detail',
   template: `
     <h3>You selected department with id = {{departmentID}}</h3>
+    <a (click)="goPrevious()">Previous</a>&nbsp;&nbsp;
+    <a (click)="goNext()">Next</a>
   `,
   styles: []
 })
@@ -12,12 +14,27 @@ export class DepartmentDetailComponent implements OnInit {
 
   public departmentID;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
 // tslint:disable-next-line: radix
-    let id = parseInt(this.route.snapshot.paramMap.get('id'));
-    this.departmentID = id;
+    //let id = parseInt(this.route.snapshot.paramMap.get('id'));
+    //this.departmentID = id;
+    this.route.paramMap.subscribe((params: ParamMap) => {
+// tslint:disable-next-line: radix
+      let id = parseInt(params.get('id'));
+      this.departmentID = id;
+     });
+  }
+
+  goPrevious(){
+    let previousId = this.departmentID - 1;
+    this.router.navigate(['/departments', previousId]);
+  }
+
+  goNext(){
+    let nextId = this.departmentID + 1;
+    this.router.navigate(['/departments', nextId]);
   }
 
 }
